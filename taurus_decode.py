2bbbb63e71a266699a752b8584f6273b7ffdf803
Lior Ben Porat <liorbp@microsoft.com>
2024-07-03 10:34:57 +0300

    Fix some bugs and add support for new offset key format

--- taurus_decode.py
+++ taurus_decode.py
@@ -2,7 +2,6 @@ import argparse
 from datetime import datetime
 import os
 import re
-import sys
 
 
 def get_letter(tab, offset):
@@ -19,25 +18,29 @@ def decode_taurus(payload, offset, delim):
 
 
 def extract_encoded_line(line, delim, func):
-# Use Regex to identify and extract the obfuscated string in the line
-# For example: 87>120>118>109>114>107>77>119>74>112>115>101>120>44>43>79>85>75>101>107>106>125>82>94>106>71>93>88>43>45
-# Use further regex to extract the "offset" key by looking for the delimeter, followed by a 1-3 character digit, comma, and finally the offset key
-# Replace the obfuscated line with the deobfuscated one
-    clean_fucn = re.escape(func)
-    clean_delim = re.escape(delim)
-    encoded_strings = re.findall(clean_fucn + '\("(?:[\d]{1,3}[' + clean_delim + ']{0,1}){1,}\",([\d]{1,3})', line)
-    key_ints = re.findall(delim + '[0-9]{1,3}",(\d)', line)
-    if len(key_ints) == len(encoded_strings):
-        i = 0
-        for i in range(len(encoded_strings)):
-            plaintext = decode_taurus(encoded_strings[i], key_ints[i], delim)
-            replacement_text = '%s("%s",%s)' % (func, encoded_strings[i], key_ints[i])
-            plaintext = '"%s"' % (plaintext)
+# Use Regex to identify and extract the obfuscated strings in the line (can be multilpe in the same line)
+# For example: DECRYPT("87>120>118>109>114>107>77>119>74>112>115>101>120>44>43>79>85>75>101>107>106>125>82>94>106>71>93>88>43>45", 1)
+# The regex also extracts the "offset" key that appears next to the encoded string. The key can appear as a single number, or a division of two numbers (e.g. 12/4)
+# Finally it will replace the obfuscated string with the deobfuscated one and return the line after all deobfuscation is done
+    try:
+        clean_func = re.escape(func)
+        clean_delim = re.escape(delim)
+        encoded_strings = re.findall(clean_func + r'\("((?:\d{1,3}' + clean_delim + r'\d{1,3})*)",([\d]{1,3})(?:\/([\d]{1,3}))?', line)
+        for encoded_string in encoded_strings:
+            if not all(encoded_string):
+                key = int(encoded_string[1])
+                replacement_text = '{}("{}",{})'.format(func, encoded_string[0], encoded_string[1])
+            else:
+                key = int(int(encoded_string[1]) / int(encoded_string[3]))
+                replacement_text = '{}("{}",{}/{})'.format(func, encoded_string[0], encoded_string[1], encoded_string[3])
+            
+            plaintext = decode_taurus(encoded_string[0], key, delim)
+            plaintext = '"{}"'.format(plaintext)
             line = line.replace(replacement_text, plaintext)
-            i = i + 1
-    else:
-        print("MISSING KEY:" + line)
-    return line
+            
+        return line
+    except:
+        print("Error on line:" + line)
 
 
 def decode_obfuscated_file(args):
@@ -54,30 +57,30 @@ def decode_obfuscated_file(args):
 
 def make_output(args):
 # Create name of output file based on date and time
-    filename = args.input + '.deobfuscated_' + datetime.utcnow().isoformat().replace(':', '.')
-    return os.path.join(args.output, filename)
+    filename = args.input + '.deobfuscated_' + datetime.now().isoformat().replace(':', '.')
+    print('Saving deobfuscated output to: {}\\{}'.format(args.output_dir, filename))
+    return os.path.join(args.output_dir, filename)
 
 
 def get_args():
 # Collect arguments. 
 # "Function" paramter should be name of malicious function used to decode strings
 # I.e. qLAEMpnctIeWhZ
-# Example command line: $ python taurus_decode.py -d ">" -i Poi.xltx -o /path/to/output -f qLAEMpnctIeWhZ
+# Example command line: $ python taurus_decode.py -d ">" -i Poi.xltx -o /path/to/output/directory -f qLAEMpnctIeWhZ
     parser = argparse.ArgumentParser(
         description='Decode strings from AutoIT Taurus Loader'
     )
     parser.add_argument('-d', '--delimiter', help='Define which delimiter should be used to split the strings on', required=True)
-    parser.add_argument('-o', '--output', help='Directory for output that exsists', required=True)
+    parser.add_argument('-o', '--output_dir', help='Directory for output that exsists', required=True)
     parser.add_argument('-i', '--input',  help='Obfuscated AutoIT file', required=True)
-    parser.add_argument('-f', '--function', help='name of function in obfuscated AutoIT which deocdes strings', required=True)
+    parser.add_argument('-f', '--function', help='Name of function in obfuscated AutoIT which deocdes strings', required=True)
     return parser.parse_args()
 
 
 def main():
 # Start and ensure the output path exsists
     args = get_args()
-    print(args.output)
-    os.makedirs(args.output, exist_ok=True)
+    os.makedirs(args.output_dir, exist_ok=True)
     decode_obfuscated_file(args)
 
 
